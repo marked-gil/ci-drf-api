791dc64aa66cc16a6c04db5b2f35d65124d5da28
marked-gil <markgil_creates@yahoo.com>
2023-02-20 00:50:05 +0000

    Fix Default_File_storage

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -21,7 +21,7 @@ CLOUDINARY_STORAGE = {
 }
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinary'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'profile'
+    'profiles'
 ]
 
 MIDDLEWARE = [
