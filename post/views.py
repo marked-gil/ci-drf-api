a6487ff659cd67b3ce67b2cbae82fdb1edb175ce
marked-gil <markgil_creates@yahoo.com>
2023-02-22 20:33:01 +0000

    Create get and post methods in PostList view

--- post/views.py
+++ post/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    # creates a nice post form in preview window
+    serializer_class = PostSerializer
+    # allow only login user to post
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        # deserialize the post data
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
