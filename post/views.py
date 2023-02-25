7edffbb6e7069e780090d714e31701a62cc0aaea
marked-gil <markgil_creates@yahoo.com>
2023-02-25 20:13:07 +0000

    Add following_id in Profile Serializer

--- post/views.py
+++ post/views.py
@@ -72,7 +72,8 @@ class PostList(generics.ListCreateAPIView):
 #         if serializer.is_valid():
 #             serializer.save()
 #             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(serializer.errors, 
+#                         status=status.HTTP_400_BAD_REQUEST)
 
 #     def delete(self, request, pk):
 #         post = self.get_object(pk)
